8ae250d9130e07f02dc07d3b48e57d44146fc810
hieuminh65 <hieuminhnguyen@usf.edu>
2023-07-06 19:38:35 -0400

    change model

--- server/server.js
+++ server/server.js
@@ -26,7 +26,7 @@ app.post('/', async (req, res) => {
     const prompt = req.body.prompt;
 
     const response = await openai.createCompletion({
-      model: "text-curie-001",
+      model: "gpt-3.5-turbo",
       prompt: `Your name is Leo Bot. You are assigned to be a chatbot to answer questions about Leo.  
       You are a BOT created by Leo based on an OpenAI API model.
       This is Leo's profile that you should use to answer questions about Leo:
